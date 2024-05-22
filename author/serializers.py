f9560106650994a10c3d5292616ae6a25a4b89e1
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-05-22 20:41:58 +0300

    Fix1

--- author/serializers.py
+++ author/serializers.py
@@ -4,6 +4,7 @@ from author.models import Author
 
 
 class AuthorSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
     first_name = serializers.CharField(max_length=64)
     last_name = serializers.CharField(max_length=64)
     pseudonym = serializers.CharField(
