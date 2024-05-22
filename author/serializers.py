e1c3c6e77e42c92b587e8a5d87b1f86cdbf3980d
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-05-22 20:45:27 +0300

    Fix3

--- author/serializers.py
+++ author/serializers.py
@@ -3,38 +3,30 @@ from rest_framework import serializers
 from author.models import Author
 
 
-class AuthorSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
+class AuthorSerializer(serializers.Serializer):
     first_name = serializers.CharField(max_length=64)
     last_name = serializers.CharField(max_length=64)
-    pseudonym = serializers.CharField(
-        max_length=64, required=False
-    )
+    pseudonym = serializers.CharField(max_length=64, required=False)
     age = serializers.IntegerField()
     retired = serializers.BooleanField()
-
-    class Meta:
-        model = Author
-        fields = "__all__"
 
     def create(self, validated_data):
         return Author.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         instance.first_name = validated_data.get(
-            "first_name", instance.first_name
+            "first_name",
+            instance.first_name
         )
         instance.last_name = validated_data.get(
-            "last_name", instance.last_name
+            "last_name",
+            instance.last_name
         )
         instance.pseudonym = validated_data.get(
-            "pseudonym", instance.pseudonym
+            "pseudonym",
+            instance.pseudonym
         )
-        instance.age = validated_data.get(
-            "age", instance.age
-        )
-        instance.retired = validated_data.get(
-            "retired", instance.retired
-        )
+        instance.age = validated_data.get("age", instance.age)
+        instance.retired = validated_data.get("retired", instance.retired)
         instance.save()
         return instance
--- author/urls.py
+++ author/urls.py
@@ -1,13 +1,12 @@
+from django.urls import include, path
 from rest_framework import routers
-from django.urls import path, include
 
-from author.views import AuthorViewSet
+from author import views
 
-
-author_router = routers.DefaultRouter()
-author_router.register("authors", AuthorViewSet, basename="manage")
-
-urlpatterns = [path("", include(author_router.urls))]
-
+router = routers.DefaultRouter()
+router.register("authors", views.AuthorViewSet, basename="manage")
 
 app_name = "author"
+urlpatterns = [
+    path("", include(router.urls))
+]
